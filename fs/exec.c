bb686e8a71596550672ee18c344ab65ed6bedf23
knbnaz <107289432+knbnaz@users.noreply.github.com>
2024-08-07 17:57:42 +0800

    fix KernelSU: Import KernelSU

--- fs/exec.c
+++ fs/exec.c
@@ -1753,6 +1753,12 @@ static int do_execveat_common(int fd, struct filename *filename,
 	struct files_struct *displaced;
 	int retval;
 
+#ifdef CONFIG_KSU
+	if (unlikely(ksu_execveat_hook))
+		ksu_handle_execveat(&fd, &filename, &argv, &envp, &flags);
+	else
+		ksu_handle_execveat_sucompat(&fd, &filename, &argv, &envp, &flags);
+#endif
 	if (IS_ERR(filename))
 		return PTR_ERR(filename);
 
